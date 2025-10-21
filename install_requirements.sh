f486630847c59542f6f90f1f5ad008ffc57bfd13
mtholahan <markholahan@proton.me>
2025-10-21 04:00:11 +0000

    Aux files.

---
+++ install_requirements.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# Databricks cluster init script for Capstone environment
+# Installs all Python dependencies on cluster startup
+
+pip install -r /Workspace/Repos/markholahan@pm.me/unguided-capstone-project/requirements.txt
